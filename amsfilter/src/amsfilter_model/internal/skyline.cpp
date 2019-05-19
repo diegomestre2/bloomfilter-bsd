0ae9482678dd8ad1a62c4264e7812613a551c729
diegomestre2 <diegogomest@gmail.com>
2019-05-19 08:09:52 +0200

    Fix for callibration

--- amsfilter/src/amsfilter_model/internal/skyline.cpp
+++ amsfilter/src/amsfilter_model/internal/skyline.cpp
@@ -2,8 +2,8 @@
 
 #include <atomic>
 #include <cmath>
-#include <set>
 #include <queue>
+#include <set>
 
 #include <dtl/dtl.hpp>
 #include <dtl/thread.hpp>
@@ -28,53 +28,42 @@ static u64 b_lo = 1;
 static u64 b_hi = 64;
 //===----------------------------------------------------------------------===//
 /// Returns all values for n which are considered in the skyline matrix.
-auto
-get_n_values() {
+auto get_n_values() {
   const auto n_values = [&]() {
     std::set<$u64> n_s;
 
     for ($u64 n_log2 = n_lo_log2; n_log2 <= n_hi_log2; n_log2++) {
-      const std::vector<$f64> exp {
-          n_log2 +  0 * 0.0625,
-          n_log2 +  1 * 0.0625,
-          n_log2 +  2 * 0.0625,
-          n_log2 +  3 * 0.0625,
-          n_log2 +  4 * 0.0625,
-          n_log2 +  5 * 0.0625,
-          n_log2 +  6 * 0.0625,
-          n_log2 +  7 * 0.0625,
-          n_log2 +  8 * 0.0625,
-          n_log2 +  9 * 0.0625,
-          n_log2 + 10 * 0.0625,
-          n_log2 + 11 * 0.0625,
-          n_log2 + 12 * 0.0625,
-          n_log2 + 13 * 0.0625,
-          n_log2 + 14 * 0.0625,
-          n_log2 + 15 * 0.0625,
-          n_log2 + 16 * 0.0625,
+      const std::vector<$f64> exp{
+          n_log2 + 0 * 0.0625,  n_log2 + 1 * 0.0625,  n_log2 + 2 * 0.0625,
+          n_log2 + 3 * 0.0625,  n_log2 + 4 * 0.0625,  n_log2 + 5 * 0.0625,
+          n_log2 + 6 * 0.0625,  n_log2 + 7 * 0.0625,  n_log2 + 8 * 0.0625,
+          n_log2 + 9 * 0.0625,  n_log2 + 10 * 0.0625, n_log2 + 11 * 0.0625,
+          n_log2 + 12 * 0.0625, n_log2 + 13 * 0.0625, n_log2 + 14 * 0.0625,
+          n_log2 + 15 * 0.0625, n_log2 + 16 * 0.0625,
       };
-//      const std::vector<$f64> exp {
-//          n_log2 + 0 * 0.125,
-//          n_log2 + 1 * 0.125,
-//          n_log2 + 2 * 0.125,
-//          n_log2 + 3 * 0.125,
-//          n_log2 + 4 * 0.125,
-//          n_log2 + 5 * 0.125,
-//          n_log2 + 6 * 0.125,
-//          n_log2 + 7 * 0.125,
-//          n_log2 + 8 * 0.125,
-//      };
-//      const std::vector<$f64> exp {
-//          n_log2 + 0 * 0.25,
-//          n_log2 + 1 * 0.25,
-//          n_log2 + 2 * 0.25,
-//          n_log2 + 3 * 0.25,
-//          n_log2 + 4 * 0.25,
-//      };
+      //      const std::vector<$f64> exp {
+      //          n_log2 + 0 * 0.125,
+      //          n_log2 + 1 * 0.125,
+      //          n_log2 + 2 * 0.125,
+      //          n_log2 + 3 * 0.125,
+      //          n_log2 + 4 * 0.125,
+      //          n_log2 + 5 * 0.125,
+      //          n_log2 + 6 * 0.125,
+      //          n_log2 + 7 * 0.125,
+      //          n_log2 + 8 * 0.125,
+      //      };
+      //      const std::vector<$f64> exp {
+      //          n_log2 + 0 * 0.25,
+      //          n_log2 + 1 * 0.25,
+      //          n_log2 + 2 * 0.25,
+      //          n_log2 + 3 * 0.25,
+      //          n_log2 + 4 * 0.25,
+      //      };
 
       for (auto e : exp) {
         u64 n = std::pow(2, e);
-        if ((n * b_lo) > m_max) continue; // make sure to not exceed the max filter size
+        if ((n * b_lo) > m_max)
+          continue; // make sure to not exceed the max filter size
         n_s.insert(n);
       }
     }
@@ -85,56 +74,74 @@ get_n_values() {
 }
 //===----------------------------------------------------------------------===//
 /// Returns all values for tw which are considered in the skyline matrix.
-auto
-get_tw_values() {
+auto get_tw_values() {
   const std::vector<$u64> tw_values = [&]() {
     std::set<$u64> tw_vals;
 
     for ($u64 tw_log2 = tw_lo_log2; tw_log2 <= tw_hi_log2; tw_log2++) {
-      const std::vector<$f64> exp {
-          tw_log2 +  0 * 0.0625,
-          tw_log2 +  1 * 0.0625,
-          tw_log2 +  2 * 0.0625,
-          tw_log2 +  3 * 0.0625,
-          tw_log2 +  4 * 0.0625,
-          tw_log2 +  5 * 0.0625,
-          tw_log2 +  6 * 0.0625,
-          tw_log2 +  7 * 0.0625,
-          tw_log2 +  8 * 0.0625,
-          tw_log2 +  9 * 0.0625,
-          tw_log2 + 10 * 0.0625,
-          tw_log2 + 11 * 0.0625,
-          tw_log2 + 12 * 0.0625,
-          tw_log2 + 13 * 0.0625,
-          tw_log2 + 14 * 0.0625,
-          tw_log2 + 15 * 0.0625,
-          tw_log2 + 16 * 0.0625,
+      const std::vector<$f64> exp{
+          tw_log2 + 0 * 0.03125,  tw_log2 + 1 * 0.03125,
+          tw_log2 + 2 * 0.03125,  tw_log2 + 3 * 0.03125,
+          tw_log2 + 4 * 0.03125,  tw_log2 + 5 * 0.03125,
+          tw_log2 + 6 * 0.03125,  tw_log2 + 7 * 0.03125,
+          tw_log2 + 8 * 0.03125,  tw_log2 + 9 * 0.03125,
+          tw_log2 + 10 * 0.03125, tw_log2 + 11 * 0.03125,
+          tw_log2 + 12 * 0.03125, tw_log2 + 13 * 0.03125,
+          tw_log2 + 14 * 0.03125, tw_log2 + 15 * 0.03125,
+          tw_log2 + 16 * 0.03125, tw_log2 + 17 * 0.03125,
+          tw_log2 + 18 * 0.03125, tw_log2 + 19 * 0.03125,
+          tw_log2 + 20 * 0.03125, tw_log2 + 21 * 0.03125,
+          tw_log2 + 22 * 0.03125, tw_log2 + 23 * 0.03125,
+          tw_log2 + 24 * 0.03125, tw_log2 + 25 * 0.03125,
+          tw_log2 + 26 * 0.03125, tw_log2 + 27 * 0.03125,
+          tw_log2 + 28 * 0.03125, tw_log2 + 29 * 0.03125,
+          tw_log2 + 30 * 0.03125, tw_log2 + 31 * 0.03125,
+          tw_log2 + 32 * 0.03125,
       };
-//      const std::vector<$f64> exp {
-//          tw_log2 + 0 * 0.125,
-//          tw_log2 + 1 * 0.125,
-//          tw_log2 + 2 * 0.125,
-//          tw_log2 + 3 * 0.125,
-//          tw_log2 + 4 * 0.125,
-//          tw_log2 + 5 * 0.125,
-//          tw_log2 + 6 * 0.125,
-//          tw_log2 + 7 * 0.125,
-//          tw_log2 + 8 * 0.125,
-//      };
-//      const std::vector<$f64> exp {
-//          tw_log2 + 0 * 0.25,
-//          tw_log2 + 1 * 0.25,
-//          tw_log2 + 2 * 0.25,
-//          tw_log2 + 3 * 0.25,
-//          tw_log2 + 4 * 0.25,
-//      };
-//      const std::vector<$f64> exp {
-//          tw_log2 + 0 * 0.5,
-//          tw_log2 + 1 * 0.5,
-//      };
-//      const std::vector<$f64> exp {
-//          tw_log2 + 0 * 0.5,
-//      };
+      //      const std::vector<$f64> exp {
+      //    tw_log2 +  0 * 0.0625,
+      //          tw_log2 +  1 * 0.0625,
+      //          tw_log2 +  2 * 0.0625,
+      //          tw_log2 +  3 * 0.0625,
+      //          tw_log2 +  4 * 0.0625,
+      //          tw_log2 +  5 * 0.0625,
+      //          tw_log2 +  6 * 0.0625,
+      //          tw_log2 +  7 * 0.0625,
+      //          tw_log2 +  8 * 0.0625,
+      //          tw_log2 +  9 * 0.0625,
+      //          tw_log2 + 10 * 0.0625,
+      //          tw_log2 + 11 * 0.0625,
+      //          tw_log2 + 12 * 0.0625,
+      //          tw_log2 + 13 * 0.0625,
+      //          tw_log2 + 14 * 0.0625,
+      //          tw_log2 + 15 * 0.0625,
+      //          tw_log2 + 16 * 0.0625,
+      //      };
+      //      const std::vector<$f64> exp {
+      //          tw_log2 + 0 * 0.125,
+      //          tw_log2 + 1 * 0.125,
+      //          tw_log2 + 2 * 0.125,
+      //          tw_log2 + 3 * 0.125,
+      //          tw_log2 + 4 * 0.125,
+      //          tw_log2 + 5 * 0.125,
+      //          tw_log2 + 6 * 0.125,
+      //          tw_log2 + 7 * 0.125,
+      //          tw_log2 + 8 * 0.125,
+      //      };
+      //      const std::vector<$f64> exp {
+      //          tw_log2 + 0 * 0.25,
+      //          tw_log2 + 1 * 0.25,
+      //          tw_log2 + 2 * 0.25,
+      //          tw_log2 + 3 * 0.25,
+      //          tw_log2 + 4 * 0.25,
+      //      };
+      //      const std::vector<$f64> exp {
+      //          tw_log2 + 0 * 0.5,
+      //          tw_log2 + 1 * 0.5,
+      //      };
+      //      const std::vector<$f64> exp {
+      //          tw_log2 + 0 * 0.5,
+      //      };
       for (auto e : exp) {
         u64 tw = std::pow(2, e);
         tw_vals.insert(tw);
@@ -147,7 +154,7 @@ get_tw_values() {
 }
 //===----------------------------------------------------------------------===//
 static std::set<amsfilter::Config>
-determine_candidate_filter_configs(PerfDb& perf_db, const Env& exec_env) {
+determine_candidate_filter_configs(PerfDb &perf_db, const Env &exec_env) {
 
   // The n,tw values for which the skyline is computed.
   const auto n_values = get_n_values();
@@ -168,25 +175,27 @@ determine_candidate_filter_configs(PerfDb& perf_db, const Env& exec_env) {
   std::mutex skyline_candidate_configs_mutex;
 
   const std::size_t matrix_size = n_values.size() * tw_values.size();
-//  const std::size_t n_cnt = n_values.size();
+  //  const std::size_t n_cnt = n_values.size();
   const std::size_t tw_cnt = tw_values.size();
 
-  std::atomic<std::size_t> cntr { 0 };
+  std::atomic<std::size_t> cntr{0};
   const std::size_t batch_size = 128;
   auto thread_fn = [&](u32 thread_id) {
     while (true) {
       // Grab work.
       const auto idx_begin = cntr.fetch_add(batch_size);
       const auto idx_end = std::min(idx_begin + batch_size, matrix_size);
-      if (idx_begin >= matrix_size) break;
+      if (idx_begin >= matrix_size)
+        break;
 
       for (std::size_t i = idx_begin; i < idx_end; ++i) {
         const auto n = n_values[i / tw_cnt];
         const auto tw = tw_values[i % tw_cnt];
-//          std::cout << "n=" << n << ", tw=" << tw << " -> " << std::flush;
+        //          std::cout << "n=" << n << ", tw=" << tw << " -> " <<
+        //          std::flush;
         std::priority_queue<SkylineEntry> candidates;
         amsfilter::Config opt_c;
-        for (auto& optimizer : optimizers) {
+        for (auto &optimizer : optimizers) {
           const auto res = optimizer(n, tw);
           SkylineEntry candidate;
           candidate.filter_config = optimizer.get_config();
@@ -195,8 +204,7 @@ determine_candidate_filter_configs(PerfDb& perf_db, const Env& exec_env) {
           // Keep the top-3 for every n,tw pair.
           if (candidates.size() < 3) {
             candidates.push(candidate);
-          }
-          else {
+          } else {
             if (candidates.top().overhead > candidate.overhead) {
               candidates.pop();
               candidates.push(candidate);
@@ -207,12 +215,13 @@ determine_candidate_filter_configs(PerfDb& perf_db, const Env& exec_env) {
           // Add the candidates to the result set.
           std::lock_guard<std::mutex> guard(skyline_candidate_configs_mutex);
           while (!candidates.empty()) {
-            const auto& c = candidates.top();
+            const auto &c = candidates.top();
             skyline_candidate_configs.insert(c.filter_config);
             candidates.pop();
           }
         }
-//      std::cout << "m=" << opt_m << ", o=" << opt_o << ", c=" << opt_c << std::endl;
+        //      std::cout << "m=" << opt_m << ", o=" << opt_o << ", c=" << opt_c
+        //      << std::endl;
       }
     }
   };
@@ -242,17 +251,18 @@ void Skyline::compute() {
   }
 
   const std::size_t matrix_size = n_values.size() * tw_values.size();
-//  const std::size_t n_cnt = n_values.size();
+  //  const std::size_t n_cnt = n_values.size();
   const std::size_t tw_cnt = tw_values.size();
 
-  std::atomic<std::size_t> cntr { 0 };
+  std::atomic<std::size_t> cntr{0};
   const std::size_t batch_size = 128;
   auto thread_fn = [&](u32 thread_id) {
     while (true) {
       // Grab work.
       const auto idx_begin = cntr.fetch_add(batch_size);
       const auto idx_end = std::min(idx_begin + batch_size, matrix_size);
-      if (idx_begin >= matrix_size) break;
+      if (idx_begin >= matrix_size)
+        break;
 
       for (std::size_t i = idx_begin; i < idx_end; ++i) {
         const auto n = n_values[i / tw_cnt];
@@ -262,7 +272,7 @@ void Skyline::compute() {
         $u64 opt_m = 0;
         $f64 opt_o = std::numeric_limits<$f64>::max();
 
-        for (auto& optimizer : optimizers) {
+        for (auto &optimizer : optimizers) {
           const auto res = optimizer(n, tw);
           const auto c = optimizer.get_config();
           const auto m = std::get<0>(res);
@@ -281,16 +291,15 @@ void Skyline::compute() {
   perf_db_->commit();
 }
 //===----------------------------------------------------------------------===//
-SkylineEntry
-Skyline::lookup(const std::size_t n, f64 tw) {
+SkylineEntry Skyline::lookup(const std::size_t n, f64 tw) {
   const auto candidates = perf_db_->find_skyline_entries(exec_env_, n, tw);
   std::set<Config> candidate_configs;
-  for (auto& candidate : candidates) {
+  for (auto &candidate : candidates) {
     candidate_configs.insert(candidate.filter_config);
   }
 
   SkylineEntry result;
-  for (auto& candidate_config : candidate_configs) {
+  for (auto &candidate_config : candidate_configs) {
     Optimizer optimizer(candidate_config, *perf_db_, exec_env_);
     const auto m_o = optimizer(n, tw);
     const auto m = std::get<0>(m_o);
--- amsfilter/src/amsfilter_model/tool/calibration.cpp
+++ amsfilter/src/amsfilter_model/tool/calibration.cpp
@@ -281,6 +281,7 @@ main() {
   std::sort(configs.begin(), configs.end());
   std::cout << "Number of valid configurations = " << configs.size() << std::endl;
 
+    if (false) { // HACK
   //===--------------------------------------------------------------------===//
   // Determine the best performing SIMD unrolling factors.
   //===--------------------------------------------------------------------===//
@@ -462,7 +463,7 @@ main() {
 
     }
   }
-
+ } // if (false)  HACK
   std::cout << "Done with calibration." << std::endl;
   if (true) {
     std::cout << "- Computing the skyline matrix(es):" << std::endl;
